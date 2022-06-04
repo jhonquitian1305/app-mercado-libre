7076bbdadb41c1436882f3d5c3e744f56938748d
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-06-04 14:33:28 -0500

    Adding search items

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ItemsService } from 'src/app/services/items.service';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  formSearch: FormGroup;
+
+  arrItems: any[] = [];
+
+  constructor(
+    private itemsService: ItemsService
+  ) {
+    this.formSearch = new FormGroup({
+      search: new FormControl('')
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSearch(){
+    let search = this.formSearch.value
+    console.log(search.search)
+    this.itemsService.getItems(search.search)
+      .then(async response => {
+        this.arrItems = await response['results']
+        console.log(this.arrItems);
+      })
+  }
+
+}
